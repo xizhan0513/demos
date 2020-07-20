b04c11d5b811391a5ca2080ddcecd0d2fe1948e5
xizhan <xizhan@nationalchip.com>
2020-07-20 19:26:40 +0800

    update kthread demo

--- kthread/kthread.c
+++ kthread/kthread.c
@@ -10,7 +10,7 @@ static char data[] = "test for kthread";
 
 static int demo_kthread_func(void *data)
 {
-	int time_count = 30;
+	int time_count = 10;
 
 	while (!kthread_should_stop() && (time_count > 0)) {
 		printk(KERN_INFO "PID: %d; NAEM: %s\n", current->pid, current->comm);
@@ -34,13 +34,17 @@ static int __init demo_init(void)
 	return 0;
 }
 
+/*
+	1、调用kthread_stop()之后，一直阻塞，直到线程退出(所以不要在线程中调用kthread_stop()，不然会死锁)。
+	2、调用kthread_stop()之后，kthread_should_stop()会返回true。
+*/
 static void __exit demo_exit(void)
 {
 	int ret = 0;
 
 	if (!IS_ERR(demo_task)) {
 		ret = kthread_stop(demo_task);
-		printk("demo kthread has run %ds\n", ret);
+		printk("kthread_stop, ret = %d\n", ret);
 	}
 
 	printk("Exit done!\n");
