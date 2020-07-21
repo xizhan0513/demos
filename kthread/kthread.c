8fd5b1e4137409799edb4ba5c673b09e3b23d1fa
xizhan <xizhan@nationalchip.com>
2020-07-21 20:25:39 +0800

    update wait_queue demo

--- kthread/kthread.c
+++ kthread/kthread.c
@@ -5,6 +5,7 @@
 #include <linux/delay.h>
 #include <linux/kthread.h>
 
+static int kthread_exit_flags = false;
 static struct task_struct *demo_task;
 static char data[] = "test for kthread";
 
@@ -18,7 +19,9 @@ static int demo_kthread_func(void *data)
 		msleep(1000);
 	}
 
-	return 0;
+	kthread_exit_flags = true;
+
+	return 13;
 }
 
 static int __init demo_init(void)
@@ -35,14 +38,16 @@ static int __init demo_init(void)
 }
 
 /*
-	1、调用kthread_stop()之后，一直阻塞，直到线程退出(所以不要在线程中调用kthread_stop()，不然会死锁)。
-	2、调用kthread_stop()之后，kthread_should_stop()会返回true。
+	1、调用kthread_stop()之后，首先唤醒线程，然后一直阻塞，直到线程退出(所以不要在线程中调用kthread_stop()，不然会死锁)。
+	2、调用kthread_stop()之后，设置线程的退出标记，kthread_should_stop()会返回true(所以要在线程中调用kthread_should_stop()进行判断)。
+	3、一般线程退出后，调用do_exit()释放一些系统资源；需要调用kthread_stop()彻底释放内核线程所占用的系统资源。
+	注：调用kthread_stop()时，线程必须还没有退出，不然会出现Oops的问题。
 */
 static void __exit demo_exit(void)
 {
 	int ret = 0;
 
-	if (!IS_ERR(demo_task)) {
+	if (!IS_ERR(demo_task) && (kthread_exit_flags == false)) {
 		ret = kthread_stop(demo_task);
 		printk("kthread_stop, ret = %d\n", ret);
 	}
--- wait_queue/wait_queue.c
+++ wait_queue/wait_queue.c
@@ -5,9 +5,11 @@
 #include <linux/sched.h>
 #include <linux/delay.h>
 #include <linux/wait.h>
+#include <linux/slab.h>
 #include <linux/kthread.h>
 
-static int flags = false;
+static int wait_flags = false;
+static int thread_exit_flags = false;
 static struct task_struct *demo_task;
 wait_queue_head_t wqh;
 
@@ -15,8 +17,10 @@ wait_queue_head_t wqh;
 static int demo_kthread_func(void *data)
 {
 	printk("wait_event start!\n");
-	wait_event(wqh, flags == true);
+	wait_event(wqh, wait_flags == true);
 	printk("wait_event done!\n");
+
+	thread_exit_flags = true;
 
 	return 0;
 }
@@ -33,10 +37,6 @@ static int __init demo_init(void)
 
 	printk("Init done!\n");
 
-	msleep(5000);
-	flags = true;
-	wake_up(&wqh);
-
 	return 0;
 }
 
@@ -44,9 +44,12 @@ static void __exit demo_exit(void)
 {
 	int ret = 0;
 
-	if (!IS_ERR(demo_task)) {
+	wait_flags = true;
+	wake_up(&wqh);
+
+	if (!IS_ERR(demo_task) && (thread_exit_flags == false)) {
 		ret = kthread_stop(demo_task);
-		printk("demo kthread has run %ds\n", ret);
+		printk("kthread_stop, ret = %d\n", ret);
 	}
 
 	printk("Exit done!\n");
@@ -58,3 +61,203 @@ module_init(demo_init);
 module_exit(demo_exit);
 
 MODULE_LICENSE("GPL");
+
+/* 以下来自osal对wait_queue的封装 */
+typedef int (*osal_wait_cond_func_t)(const void *param);
+typedef struct osal_wait {
+    void *wait;
+} osal_wait_t;
+
+unsigned long osal_msecs_to_jiffies(const unsigned int m)
+{
+    return msecs_to_jiffies(m);
+}
+EXPORT_SYMBOL(osal_msecs_to_jiffies);
+
+int osal_wait_init(osal_wait_t *wait)
+{
+    wait_queue_head_t *wq = NULL;
+    if (wait == NULL) {
+        printk("%s - parameter invalid!\n", __FUNCTION__);
+        return -1;
+    }
+    wq = (wait_queue_head_t *)kmalloc(sizeof(wait_queue_head_t), GFP_ATOMIC);
+    if (wq == NULL) {
+        printk("%s - kmalloc error!\n", __FUNCTION__);
+        return -1;
+    }
+    init_waitqueue_head(wq);
+    wait->wait = wq;
+    return 0;
+}
+EXPORT_SYMBOL(osal_wait_init);
+
+/* 如果在调用此接口前， func(param)返回true，则该接口不阻塞 */
+int osal_wait_interruptible(osal_wait_t *wait, osal_wait_cond_func_t func, void *param)
+{
+    wait_queue_head_t *wq = NULL;
+    DEFINE_WAIT(__wait);
+    long ret = 0;
+    int condition = 0;
+
+    if (wait == NULL) {
+        printk("%s - parameter invalid!\n", __FUNCTION__);
+        return -1;
+    }
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return -1;
+    }
+    prepare_to_wait(wq, &__wait, TASK_INTERRUPTIBLE);
+    /* if wakeup the queue brefore prepare_to_wait, the func will return true. And will not go to schedule */
+    if (func != NULL) {
+        condition = func(param);
+    }
+
+    if (!condition) {
+        if (!signal_pending(current)) {
+            schedule();
+        }
+        if (signal_pending(current)) {
+            ret = -ERESTARTSYS;
+        }
+    }
+
+    finish_wait(wq, &__wait);
+    return ret;
+}
+EXPORT_SYMBOL(osal_wait_interruptible);
+
+int osal_wait_uninterruptible(osal_wait_t *wait, osal_wait_cond_func_t func, void *param)
+{
+    wait_queue_head_t *wq = NULL;
+    DEFINE_WAIT(__wait);
+    long ret = 0;
+    int condition = 0;
+
+    if (wait == NULL) {
+        printk("%s - parameter invalid!\n", __FUNCTION__);
+        return -1;
+    }
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return -1;
+    }
+    prepare_to_wait(wq, &__wait, TASK_UNINTERRUPTIBLE);
+    /* if wakeup the queue brefore prepare_to_wait, the func will return true. And will not go to schedule */
+    if (func != NULL) {
+        condition = func(param);
+    }
+
+    if (!condition) {
+        schedule();
+    }
+
+    finish_wait(wq, &__wait);
+    return ret;
+}
+EXPORT_SYMBOL(osal_wait_uninterruptible);
+
+int osal_wait_timeout_interruptible(osal_wait_t *wait, osal_wait_cond_func_t func, void *param, unsigned long ms)
+{
+    wait_queue_head_t *wq = NULL;
+    DEFINE_WAIT(__wait);
+    long ret = ms;
+    int condition = 0;
+
+    if (wait == NULL) {
+        printk("%s - parameter invalid!\n", __FUNCTION__);
+        return -1;
+    }
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return -1;
+    }
+    prepare_to_wait(wq, &__wait, TASK_INTERRUPTIBLE);
+    /* if wakeup the queue brefore prepare_to_wait, the func will return true. And will not go to schedule */
+    if (func != NULL) {
+        condition = func(param);
+    }
+
+    if (!condition) {
+        if (!signal_pending(current)) {
+            ret = schedule_timeout(msecs_to_jiffies(ret));
+            ret = jiffies_to_msecs(ret);
+        }
+        if (signal_pending(current)) {
+            ret = -ERESTARTSYS;
+        }
+    }
+
+    finish_wait(wq, &__wait);
+
+    return ret;
+}
+
+EXPORT_SYMBOL(osal_wait_timeout_interruptible);
+
+int osal_wait_timeout_uninterruptible(osal_wait_t *wait, osal_wait_cond_func_t func, void *param, unsigned long ms)
+{
+    wait_queue_head_t *wq = NULL;
+    DEFINE_WAIT(__wait);
+    long ret = ms;
+    int condition = 0;
+
+    if (wait == NULL) {
+        printk("%s - parameter invalid!\n", __FUNCTION__);
+        return -1;
+    }
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return -1;
+    }
+    prepare_to_wait(wq, &__wait, TASK_UNINTERRUPTIBLE);
+    /* if wakeup the queue brefore prepare_to_wait, the func will return true. And will not go to schedule */
+    if (func != NULL) {
+        condition = func(param);
+    }
+
+    if (!condition) {
+        ret = schedule_timeout(msecs_to_jiffies(ret));
+        ret = jiffies_to_msecs(ret);
+    }
+
+    finish_wait(wq, &__wait);
+
+    return ret;
+}
+EXPORT_SYMBOL(osal_wait_timeout_uninterruptible);
+
+void osal_wakeup(osal_wait_t *wait)
+{
+    wait_queue_head_t *wq = NULL;
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return;
+    }
+    wake_up_all(wq);
+}
+EXPORT_SYMBOL(osal_wakeup);
+void osal_wait_destory(osal_wait_t *wait)
+{
+    wait_queue_head_t *wq = NULL;
+
+    wq = (wait_queue_head_t *)(wait->wait);
+    if (wq == NULL) {
+        printk("%s - wait->wait is NULL!\n", __FUNCTION__);
+        return;
+    }
+    kfree(wq);
+    wait->wait = NULL;
+}
+EXPORT_SYMBOL(osal_wait_destory);
